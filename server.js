791835d6f5fd17c54c765fe9c578d1b58b269365
Prasad Medisetti <prasadmedisetti34@gmail.com>
2021-06-15 14:11:47 +0530

    cors issue fix trail2

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.options('*', cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(
