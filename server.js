e5ce5124f93648f23e79abccf7b5ff497a59d093
Prasad Medisetti <prasadmedisetti34@gmail.com>
2021-06-15 18:40:03 +0530

    cors issue fixed forgot to add TOKEN_SECRET config variable in heroku

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(
